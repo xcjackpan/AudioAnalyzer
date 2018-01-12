ffe1687f0e370a42b0259a21d0c8a36bc76170d4
xcjackpan <xcjackpan99@gmail.com>
2018-01-11 23:15:32 -0500

    Vary colors

--- script.js
+++ script.js
@@ -183,10 +183,10 @@ file.onchange = function() {
 		canvasContext.fillRect(0, 0, cWidth, cHeight);
 
 		if (pulseB) {
-			for (var i = 0; i < 64; i += 4) {
+			for (var i = 0; i < 64; i += 6) {
 				radius = dataArray[i];
 
-				var r = 25 * (i/2);
+				var r = 25 * (i/3);
 				var g = 50;
 				var b = 50 * (radius/2);
 
